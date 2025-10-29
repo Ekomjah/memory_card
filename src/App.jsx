c301938f02f83c40c6f797a15370a90f79d31280
Ekomjah <ekomjahedet@gmail.com>
2025-10-29 18:02:22 +0100

    refactor: adds commitizen for better commit linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const key = setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    return () => {
+      clearTimeout(key);
+    };
+  }, [count]);
 
   return (
     <>
@@ -18,9 +26,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div>
+          <span>
+            {count >= 60 &&
+              `${Math.floor(count / 60)} ${Math.floor(count / 60) > 1 ? " minutes : " : " minute : "}`}
+           
+          </span>
+          <span> {count % 60} seconds</span>
+        </div>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +42,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
